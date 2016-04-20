5a386e140f4de4f7ce6fab94faa33d9b4d35cbde
Shayne Chow <shayne@weplus.cn>
2016-04-20 16:50:00 +0800

    Initial Commit

---
+++ ZXQrCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZXQrCode
+//
+//  Created by Xiang on 16/4/20.
+//  Copyright © 2016年 周想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
